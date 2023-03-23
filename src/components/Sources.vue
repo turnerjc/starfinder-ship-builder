eaa3d6b8fcdeadf98a6d1415f00770d445f83000
Denny Wright <dennyw17@gmail.com>
2023-03-22 22:06:40 -0600

    Remove unused computed from vue

--- src/components/Sources.vue
+++ src/components/Sources.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import { defineProps, computed } from 'vue';
+  import { defineProps } from 'vue';
 
   const props = defineProps(['sourceBooks', 'params']);
   console.log(props);
