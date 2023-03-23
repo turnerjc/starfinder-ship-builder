b16b82b24a2d38c5627e65bc8aef92fd1a6a11ab
Denny Wright <dennyw17@gmail.com>
2023-03-23 10:06:22 -0600

    Add Concept section inline

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script src="./app.js"></script>
 <template>
   <!--
-	| - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	|	APP
-	| - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	-->
+  | - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  | APP
+  | - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  -->
   <div id="app" class="wrapper">
     <Graphics></Graphics>
 
@@ -51,11 +51,11 @@
       -->
       <Sources :sourceBooks="sourceBooks" :params="params"></Sources>
       <!--
-			| - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-			|	INPUT
-			| - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-			-->
-
+      | - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+      | INPUT
+      | - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+      <Input />
+      -->
       <div class="box">
         <header class="box__header">
           <h2 id="input">Input</h2>
@@ -129,8 +129,52 @@
         </div>
       </div>
       <!--
-      <Input />
+      | - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+      | CONCEPT
+      | - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
       <Concept />
+      -->
+
+      <div class="box">
+        <header class="box__header">
+          <h2 id="concept">Step 0: Concept</h2>
+        </header>
+        <div class="box__select">
+          <div class="form-group">
+            <label for="shipName">Ship Name or Class</label>
+            <input
+              type="text"
+              id="shipName"
+              v-model="params.shipName"
+              class="form-control input-lg" />
+          </div>
+          <div class="form-group">
+            <label for="shipConcept">Concept</label>
+            <textarea
+              id="shipConcept"
+              cols="30"
+              rows="3"
+              v-model="params.shipConcept"
+              class="form-control"></textarea>
+          </div>
+          <div class="checkbox">
+            <label>
+              <input type="checkbox" id="isUseStrictRules" v-model="params.isUseStrictRules" />
+              Use strict ship building rules from <em>Starfinder Core Rules</em>?
+            </label>
+          </div>
+          <div class="checkbox">
+            <label>
+              <input
+                type="checkbox"
+                id="isSetDefaultCrewSkillValues"
+                v-model="params.isSetDefaultCrewSkillValues" />
+              Automatically update crew skill ranks when levelling up?
+            </label>
+          </div>
+        </div>
+      </div>
+      <!--
       <Tier />
       <ShipFrame />
       <CustomFrame />
