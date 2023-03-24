04e306272da5b159aff5ec4a84c84156663c8215
Denny Wright <dennyw17@gmail.com>
2023-03-24 16:06:14 -0600

    Update Recent Updates

--- src/components/RecentUpdates.vue
+++ src/components/RecentUpdates.vue
@@ -1,15 +1,15 @@
 <template>
   <!--
-      | - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-      |  RECENT UPDATES
-      | - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-      -->
+  | - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  |  RECENT UPDATES
+  | - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  -->
   <div class="box">
     <p>
-      <strong>Recent Updates:</strong> Conversion to Vue3 - Denny Wright (<time
-        datetime="2023-03-20"
-        >20/03/2023</time
-      >).
+      <strong>Recent Updates:</strong> Conversion to Vue3; Created Vue components for each section
+      of the website, but only got a few working. Attempted to include other components as partial
+      html; resorted to pulling partials inline and intending to convert them to Vue components. -
+      Denny Wright (<time datetime="2023-03-24">24/03/2023</time>).
     </p>
   </div>
 </template>
