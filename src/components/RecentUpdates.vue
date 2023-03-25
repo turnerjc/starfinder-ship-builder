09e35f2516c221988dda575d312117fec5458448
Denny Wright <dennyw17@gmail.com>
2023-03-25 14:52:05 -0600

    Update RecentUpdates

--- src/components/RecentUpdates.vue
+++ src/components/RecentUpdates.vue
@@ -8,11 +8,6 @@
     <p>
       <strong>Recent Updates:</strong>
     </p>
-    <p>
-      <time datetime="2023-03-24">2023-03-24</time> - Denny Wright - Updated to use Vite for
-      development. Converted Vue2 to Vue3. Created Vue components for each section of the website,
-      but only got a few working as components. Attempted to include other components as partial
-      html. Resorted to pulling partials inline and intending to convert them to Vue components.
-    </p>
+    <p><time datetime="2023-03-24">2023-03-25</time> - Denny Wright - Hosted on GitHub Pages.</p>
   </div>
 </template>
