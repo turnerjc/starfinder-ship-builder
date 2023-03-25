97ebad89d034b9f03ee44385edd71704455d7091
Denny Wright <dennyw17@gmail.com>
2023-03-24 22:01:43 -0600

    Update recent updates

--- src/components/RecentUpdates.vue
+++ src/components/RecentUpdates.vue
@@ -6,10 +6,13 @@
   -->
   <div class="box">
     <p>
-      <strong>Recent Updates:</strong> Conversion to Vue3; Created Vue components for each section
-      of the website, but only got a few working. Attempted to include other components as partial
-      html; resorted to pulling partials inline and intending to convert them to Vue components. -
-      Denny Wright (<time datetime="2023-03-24">24/03/2023</time>).
+      <strong>Recent Updates:</strong>
+    </p>
+    <p>
+      <time datetime="2023-03-24">2023-03-24</time> - Denny Wright - Updated to use Vite for
+      development. Converted Vue2 to Vue3. Created Vue components for each section of the website,
+      but only got a few working as components. Attempted to include other components as partial
+      html. Resorted to pulling partials inline and intending to convert them to Vue components.
     </p>
   </div>
 </template>
